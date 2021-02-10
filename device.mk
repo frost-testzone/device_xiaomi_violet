fe68ca93e9bbe189d6b6170f01696be18d0b6a3d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:00:29 +0100

    violet: Build display stack
    Change-Id: I8aa97b9847fd49fb1bebd35f8193aefa7e8997e1

--- device.mk
+++ device.mk
@@ -67,6 +67,18 @@ $(call soong_config_set,camera,override_format_from_reserved,true)
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.4-service \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.memtrack-service
+
+PRODUCT_PACKAGES += \
+    gralloc.qcom \
+    hwcomposer.qcom
+
 # Media
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/media,$(TARGET_COPY_OUT_VENDOR)/etc)
