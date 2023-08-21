75051804a815291b0a10f61622c31343b8621e3a
Alexander Winkowski <dereference23@outlook.com>
2023-08-21 04:59:19 +0000

    violet: Build FM2 app
    Change-Id: I6ff5a9352c5a2723e8850d1006707cad2458cfe0

--- device.mk
+++ device.mk
@@ -94,6 +94,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.xiaomi
 
+# FM
+PRODUCT_PACKAGES += \
+    FM2
+
 # Media
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/media,$(TARGET_COPY_OUT_VENDOR)/etc)
