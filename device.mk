64fe938be8b29c9fc1418ee8573b7278c83f6381
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:29:15 +0100

    violet: Add common definitions for Qualcomm
    Change-Id: I9560fd0da00958d9349a0881b5a5f64a2c02e495

--- device.mk
+++ device.mk
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Partitions
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
