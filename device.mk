052507d07551bed10b663291c3329be677576d61
Tim Zimmermann <tim@linux4.de>
2024-11-25 19:07:15 +0000

    violet: Inherit non_ab_device.mk
    This is required to build applypatch for updating recovery.
    
    Change-Id: Ieffeed9f4fcc6a1b19f27c043164d748ce08aead

--- device.mk
+++ device.mk
@@ -9,6 +9,9 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Non A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
