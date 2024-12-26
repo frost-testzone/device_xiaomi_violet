e3699f68a66f46b2f89303f5ee9d48474776526e
Frost <159105703+Karan-Frost@users.noreply.github.com>
2024-12-26 12:12:00 +0530

    Revert "violet: Add BCR v1.63"
    This reverts commit dac3f528eea845cb8289a895a42b49af0a1fd4c0.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,5 +2,4 @@ git clone --depth=1 https://github.com/selfmusing/vendor_xiaomi_violet.git -b vi
 git clone https://github.com/selfmusing/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
 git clone --depth=1 https://github.com/selfmusing/meme_cam.git -b 15 vendor/MiuiCamera
-git clone https://github.com/selfmusing/vendor_bcr.git vendor/bcr
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.1 hardware/xiaomi
