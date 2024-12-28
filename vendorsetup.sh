47608267228389fdd2227a227357f05b2ce620eb
Frost <159105703+Karan-Frost@users.noreply.github.com>
2024-12-28 18:25:57 +0530

    violet: Clone own fork of kernel
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 git clone --depth=1 https://github.com/selfmusing/vendor_xiaomi_violet.git -b vic vendor/xiaomi/violet
-git clone https://github.com/selfmusing/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
+git clone https://github.com/veux-frost-testzone/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
 git clone --depth=1 https://github.com/selfmusing/meme_cam.git -b 15 vendor/MiuiCamera
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.1 hardware/xiaomi
