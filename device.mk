0fefc8a7b314fab83a0c53cf4c9e466fc2c549ae
LuK1337 <priv.luk@gmail.com>
2025-01-17 17:06:51 +0100

    violet: Switch to common lights HAL
    Change-Id: If515e7c3f6e5b6895076f1d8381fb2c22bd1b3de
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -254,7 +254,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light-service.lineage
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
