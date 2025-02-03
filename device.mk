d72057cf053581fc1705109fd2fb34f5db08dddc
basamaryan <basam.aryan@gmail.com>
2025-02-03 10:31:04 -0500

    violet: media: Import media_codecs.xml
    From https://github.com/LineageOS/android_device_google_sunfish/blob/b2d81d3d106ef961ee1ebd036dbbb5d1b0013807/media_codecs.xml
    
    Change-Id: Ia3905329085e72a55c9e964e07ecd8a000a913f0
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -266,6 +266,7 @@ PRODUCT_PACKAGES += \
     libc2dcolorconvert
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
     $(LOCAL_PATH)/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
