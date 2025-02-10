d6db07c876ebc515a65c1ef6cd2c1d32eae91f24
Frost <159105703+Karan-Frost@users.noreply.github.com>
2025-02-10 12:48:23 +0000

    [TMP] Revert "violet: Clone own fork of kernel"
    This reverts commit 47608267228389fdd2227a227357f05b2ce620eb.
    
    * Also add --depth=1.
    
    Change-Id: Ib79d611587a208464904c17c52ef4d884cab9a20
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 git clone --depth=1 https://github.com/veux-frost-testzone/vendor_xiaomi_violet.git -b 15 vendor/xiaomi/violet
-git clone https://github.com/veux-frost-testzone/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
+git clone --depth=1 https://github.com/selfmusing/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
 git clone --depth=1 https://github.com/selfmusing/meme_cam.git -b 15 vendor/MiuiCamera
 git clone https://github.com/frost-testzone/vendor_oneplus_dolby.git -b main vendor/oneplus/dolby
