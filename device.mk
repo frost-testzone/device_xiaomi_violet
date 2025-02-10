5a38850adab4d5ec51bfcb83e58ae75abb1907bd
Frost <159105703+Karan-Frost@users.noreply.github.com>
2025-02-10 06:00:08 +0000

    Fixup! violet: Switch to AIDL USB HAL
    This fixes commit 15ea8c4bda38b66820ae2d1e6d8275625b48ec1d.
    
    Change-Id: Icbe9271359ce864c9cd3f34f75c9536170615225
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -470,7 +470,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PACKAGES += \
     usb_compositions.conf \
     android.hardware.usb@1.3-service.dual_role_usb \
-    android.hardware.usb-service.qti \
     android.hardware.usb.gadget-service.qti
 
 PRODUCT_PACKAGES += \
