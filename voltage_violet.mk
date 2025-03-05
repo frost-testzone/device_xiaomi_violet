ff4b6cb3bf81d8262625daa68c77d6d010314b4d
Frost <159105703+Karan-Frost@users.noreply.github.com>
2025-03-05 06:50:41 +0000

    violet: Welcome to VoltageOS
    Change-Id: I51c5eac365eb499d541682472d1dc2ea30721feb
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- voltage_violet.mk
+++ voltage_violet.mk
@@ -13,11 +13,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common VoltageOS stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+
+# Bootanimation Resolution.
+TARGET_BOOT_ANIMATION_RES := 1920
+
+# VoltageOS flags.
+TARGET_FACE_UNLOCK_SUPPORTED := true
+VOLTAGE_BUILD_TYPE := OFFICIAL
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_violet
+PRODUCT_NAME := voltage_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
