30d30d2fc0ebabd9f574ee3964b22eb739553a26
Frost <159105703+Karan-Frost@users.noreply.github.com>
2025-05-02 15:09:31 +0530

    [SQUASH] violet: Remove QDCM XMLs
    Revert "violet: qdcm: Fix issues found by SebaUbuntu's QDCM validator"
    
    This reverts commit 2e824df8bf6c8b1e3c4c621deec7914b09332505.
    
    Revert "violet: Import QDCM XMLs"
    
    This reverts commit 93ee06e5d5f3571afb95a3c6fe4cb95cea4695a1.
    
    Change-Id: I63d2871e82704b12c26a96e5b08fb64ff164cc1d
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -33,10 +33,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     DSPVolumeSynchronizer
 
-# Display
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,qdcm_calib_data_*.xml,$(LOCAL_PATH)/qdcm/,$(TARGET_COPY_OUT_VENDOR)/etc/)
-
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.violet.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.violet.rc
