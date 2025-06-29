a557e13c85a74ed9ec11c1b67f5561efb64acfbd
Frost <frost.github@proton.me>
2025-06-29 12:00:32 +0530

    violet: Build Xiaomi common AIDL fingerprint service
    Change-Id: I7a9f826469282a7801c54447d971e439b3e834ea

--- device.mk
+++ device.mk
@@ -90,6 +90,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint-service.xiaomi
+
 # Media
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/media,$(TARGET_COPY_OUT_VENDOR)/etc)
@@ -102,7 +106,8 @@ PRODUCT_PACKAGES += \
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/xiaomi
 
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
